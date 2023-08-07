ae16ab02f351f1ac6435450c3bb2e6ea65e64237
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2023-08-07 18:08:02 -0500

    fix

--- src/main/java/io/trunk/jenkins/client/MetricsClient.java
+++
@@ -1,8 +0,0 @@
-package io.trunk.jenkins.client;
-
-/**
- * HTTP client for uploading activity events to Trunk.
- */
-public class MetricsClient {
-    // TODO(zaycev): add this.
-}
--- src/main/java/io/trunk/jenkins/model/AnnotatedActivityRunEvent.java
+++
@@ -1,8 +0,0 @@
-package io.trunk.jenkins.model;
-
-/**
- * JSON-serializable POJO for ActivityRunEvent.
- */
-public class AnnotatedActivityRunEvent {
-    // TODO(zaycev): Implement this.
-}
