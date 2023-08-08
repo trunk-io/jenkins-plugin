66d6c99482e4d2fad13bb2cc961cd09d48bd2c6b
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2023-08-07 20:45:42 -0500

    Fixes

--- src/main/java/io/trunk/jenkins/client/MetricsClient.java
+++ src/main/java/io/trunk/jenkins/client/MetricsClient.java
@@ -1,9 +1,19 @@
 package io.trunk.jenkins.client;
 
+import okhttp3.Request;
+
 /**
  * HTTP client for uploading activity events to Trunk.
  */
 public class MetricsClient {
 
-    // TODO(zaycev): add this.
+    public static void createEvents() {
+
+        Request req = new Request.Builder()
+                .url("https://api.trunk.io")
+                .build();
+
+        // TODO: add this.
+    }
+
 }
