c2bd383b10b3cca2d916ad84309c37e308f40f49
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2023-08-10 19:51:44 -0500

    Fixes

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [0.1.0] - 2023-8-10
+
+### Added
+
+- Initial version of the plugin.
+
+### Fixed
--- src/main/java/io/trunk/jenkins/utils/VersionUtil.java
+++ src/main/java/io/trunk/jenkins/utils/VersionUtil.java
@@ -1,7 +1,28 @@
 package io.trunk.jenkins.utils;
 
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class VersionUtil {
+
+    private static final String VERSION = loadVersionFromPom();
+
+    private static String loadVersionFromPom() {
+        try {
+            final var reader = new MavenXpp3Reader();
+            final var model = reader.read(new FileReader("pom.xml"));
+            return String.format("%s%s",
+                    model.getProperties().getProperty("revision"),
+                    model.getProperties().getProperty("changelist"));
+        } catch (IOException | XmlPullParserException ioe) {
+            throw new RuntimeException(ioe.getMessage());
+        }
+    }
+
     public static String getVersion() {
-        return "0.0.1-dev";
+        return VERSION;
     }
 }
